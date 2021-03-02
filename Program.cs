1ee791c13bbd48dcd0cb0640f34ad3d3acb756fc
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-02 20:31:44 +0000

    Complete all bulletpoints.

--- Program.cs
+++ Program.cs
@@ -7,27 +7,69 @@ namespace MadLibs
         static void Main(string[] args)
         {
             /*
-            This program ...
-            Author: ...
+            This program Mad Libs are short stories with blanks for the player to fill in that represent different parts of speech. The end result is a really hilarious and strange story.
+            Author: Szymon Warszawa
             */
 
 
             // Let the user know that the program is starting:
-
+            Console.WriteLine("Running...");
 
             // Give the Mad Lib a title:
-            string title = "TITLE";
+            string title = "Games of words";
 
             Console.WriteLine(title);
             // Define user input and variables:
+            Console.Write("Enter a name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter first adjective: ");
+            string adjOne = Console.ReadLine();
+
+            Console.Write("Enter second adjective: ");
+            string adjTwo = Console.ReadLine();
+
+            Console.Write("Enter third adjective: ");
+            string adjThree = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            string verb = Console.ReadLine();
+
+            Console.Write("Enter first noun: ");
+            string nounOne = Console.ReadLine();
+
+            Console.Write("Enter second noun: ");
+            string nounTwo = Console.ReadLine();
+
+            Console.Write("Enter an animal: ");
+            string animal = Console.ReadLine();
+
+            Console.Write("Enter a name of food: ");
+            string food = Console.ReadLine();
+
+            Console.Write("Enter a fruit: ");
+            string fruit = Console.ReadLine();
+            
+            Console.Write("Enter superhero: ");
+            string hero = Console.ReadLine();
+
+            Console.Write("Enter country: ");
+            string country = Console.ReadLine();
+
+            Console.Write("Enter dessert: ");
+            string dessert = Console.ReadLine();
+
+            Console.Write("Enter year: ");
+            string year = Console.ReadLine();
 
 
             // The template for the story:
 
-            string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
+            string story = $"This morning {name} woke up feeling {adjOne}. 'It is going to be a {adjTwo} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {nounOne}, which made all the {fruit}s very {adjThree}. Concerned, {name} texted {hero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {nounTwo}s ruled the world.";
 
 
             // Print the story:
+            Console.WriteLine(story);
 
         }
     }
